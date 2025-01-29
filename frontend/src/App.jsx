ab2d7548ac49fc34eb1211d238b801bf7445c117
Thibaut Julien <thbtjulien21@gmail.com>
2025-01-29 13:31:16 +0100

    résolution bug tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
-import "./assets/styles/app.css";
 
 function App() {
   return (
     <>
-      <h1 className="underline font-bold">Ceci est un test</h1>
+      <h1 className="font-bold underline">Ceci est un teeeest</h1>
     </>
   );
 }
