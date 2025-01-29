0d7874e576c94c77aed3d14bbaac35138bb3778c
Clean-Pick <benjamin.will98@gmail.com>
2025-01-29 12:33:51 +0100

    Fake Data init + table names updated + readme.md init

--- backend/public/db/cogip.sql
+++ backend/public/db/cogip.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 29 jan. 2025 à 10:10
+-- Généré le : mer. 29 jan. 2025 à 11:29
 -- Version du serveur : 11.5.2-MariaDB
 -- Version de PHP : 7.3.21
 
@@ -12,9 +12,9 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
@@ -28,24 +28,20 @@ SET time_zone = "+00:00";
 --
 
 DROP TABLE IF EXISTS `companies`;
-CREATE TABLE IF NOT EXISTS `companies` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `type_id` int(11) NOT NULL,
-  `country` varchar(50) NOT NULL,
-  `tva` varchar(50) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`),
-  KEY `type_id` (`type_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `companies`
---
-
-INSERT INTO `companies` (`id`, `name`, `type_id`, `country`, `tva`, `created_at`, `updated_at`) VALUES
-(1, 'Company A', 1, 'Country A', 'TVA A', '2025-01-29 10:56:50', '2025-01-29 10:56:50');
+CREATE TABLE IF NOT EXISTS `companies`
+(
+    `id`         int(11)     NOT NULL AUTO_INCREMENT,
+    `name`       varchar(50) NOT NULL,
+    `type_id`    int(11)     NOT NULL,
+    `country`    varchar(50) NOT NULL,
+    `tva`        varchar(50) NOT NULL,
+    `created_at` datetime    NOT NULL DEFAULT current_timestamp(),
+    `updated_at` datetime    NOT NULL DEFAULT current_timestamp(),
+    PRIMARY KEY (`id`),
+    KEY `type_id` (`type_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -54,24 +50,20 @@ INSERT INTO `companies` (`id`, `name`, `type_id`, `country`, `tva`, `created_at`
 --
 
 DROP TABLE IF EXISTS `contacts`;
-CREATE TABLE IF NOT EXISTS `contacts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `company_id` int(11) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `phone` varchar(50) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`),
-  KEY `company_id` (`company_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `contacts`
---
-
-INSERT INTO `contacts` (`id`, `name`, `company_id`, `email`, `phone`, `created_at`, `updated_at`) VALUES
-(1, 'Contact A', 1, 'contact.a@example.com', '1234567890', '2025-01-29 10:56:50', '2025-01-29 10:56:50');
+CREATE TABLE IF NOT EXISTS `contacts`
+(
+    `id`         int(11)     NOT NULL AUTO_INCREMENT,
+    `name`       varchar(50) NOT NULL,
+    `company_id` int(11)     NOT NULL,
+    `email`      varchar(50) NOT NULL,
+    `phone`      varchar(50) NOT NULL,
+    `created_at` datetime    NOT NULL DEFAULT current_timestamp(),
+    `updated_at` datetime    NOT NULL DEFAULT current_timestamp(),
+    PRIMARY KEY (`id`),
+    KEY `company_id` (`company_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -80,22 +72,18 @@ INSERT INTO `contacts` (`id`, `name`, `company_id`, `email`, `phone`, `created_a
 --
 
 DROP TABLE IF EXISTS `invoices`;
-CREATE TABLE IF NOT EXISTS `invoices` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ref` varchar(50) NOT NULL,
-  `id_company` int(11) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`),
-  KEY `id_company` (`id_company`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `invoices`
---
-
-INSERT INTO `invoices` (`id`, `ref`, `id_company`, `created_at`, `updated_at`) VALUES
-(1, 'Invoice 1', 1, '2025-01-29 10:56:50', '2025-01-29 10:56:50');
+CREATE TABLE IF NOT EXISTS `invoices`
+(
+    `id`         int(11)     NOT NULL AUTO_INCREMENT,
+    `ref`        varchar(50) NOT NULL,
+    `company_id` int(11)     NOT NULL,
+    `created_at` datetime    NOT NULL DEFAULT current_timestamp(),
+    `updated_at` datetime    NOT NULL DEFAULT current_timestamp(),
+    PRIMARY KEY (`id`),
+    KEY `company_id` (`company_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -104,20 +92,16 @@ INSERT INTO `invoices` (`id`, `ref`, `id_company`, `created_at`, `updated_at`) V
 --
 
 DROP TABLE IF EXISTS `permissions`;
-CREATE TABLE IF NOT EXISTS `permissions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `permissions`
---
-
-INSERT INTO `permissions` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'Read User', '2025-01-29 10:52:11', '2025-01-29 10:52:11');
+CREATE TABLE IF NOT EXISTS `permissions`
+(
+    `id`         int(11)     NOT NULL AUTO_INCREMENT,
+    `name`       varchar(50) NOT NULL,
+    `created_at` datetime    NOT NULL DEFAULT current_timestamp(),
+    `updated_at` datetime    NOT NULL DEFAULT current_timestamp(),
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -126,20 +110,16 @@ INSERT INTO `permissions` (`id`, `name`, `created_at`, `updated_at`) VALUES
 --
 
 DROP TABLE IF EXISTS `roles`;
-CREATE TABLE IF NOT EXISTS `roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `roles`
---
-
-INSERT INTO `roles` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'Administrator', '2025-01-29 10:52:11', '2025-01-29 10:52:11');
+CREATE TABLE IF NOT EXISTS `roles`
+(
+    `id`         int(11)     NOT NULL AUTO_INCREMENT,
+    `name`       varchar(50) NOT NULL,
+    `created_at` datetime    NOT NULL DEFAULT current_timestamp(),
+    `updated_at` datetime    NOT NULL DEFAULT current_timestamp(),
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -148,21 +128,17 @@ INSERT INTO `roles` (`id`, `name`, `created_at`, `updated_at`) VALUES
 --
 
 DROP TABLE IF EXISTS `roles_permission`;
-CREATE TABLE IF NOT EXISTS `roles_permission` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `permission_id` int(11) NOT NULL,
-  `role_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `permission_id` (`permission_id`),
-  KEY `role_id` (`role_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `roles_permission`
---
-
-INSERT INTO `roles_permission` (`id`, `permission_id`, `role_id`) VALUES
-(1, 1, 1);
+CREATE TABLE IF NOT EXISTS `roles_permission`
+(
+    `id`            int(11) NOT NULL AUTO_INCREMENT,
+    `permission_id` int(11) NOT NULL,
+    `role_id`       int(11) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `permission_id` (`permission_id`),
+    KEY `role_id` (`role_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -171,20 +147,16 @@ INSERT INTO `roles_permission` (`id`, `permission_id`, `role_id`) VALUES
 --
 
 DROP TABLE IF EXISTS `types`;
-CREATE TABLE IF NOT EXISTS `types` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `types`
---
-
-INSERT INTO `types` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'Type 1', '2025-01-29 10:56:50', '2025-01-29 10:56:50');
+CREATE TABLE IF NOT EXISTS `types`
+(
+    `id`         int(11)     NOT NULL AUTO_INCREMENT,
+    `name`       varchar(50) NOT NULL,
+    `created_at` datetime    NOT NULL DEFAULT current_timestamp(),
+    `updated_at` datetime    NOT NULL DEFAULT current_timestamp(),
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -193,25 +165,21 @@ INSERT INTO `types` (`id`, `name`, `created_at`, `updated_at`) VALUES
 --
 
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(50) NOT NULL,
-  `role_id` int(11) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`),
-  KEY `role_id` (`role_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `users`
---
-
-INSERT INTO `users` (`id`, `first_name`, `role_id`, `last_name`, `email`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'John', 1, 'Doe', 'john.doe@example.com', 'password123', '2025-01-29 10:52:11', '2025-01-29 10:52:11');
+CREATE TABLE IF NOT EXISTS `users`
+(
+    `id`         int(11)     NOT NULL AUTO_INCREMENT,
+    `first_name` varchar(50) NOT NULL,
+    `role_id`    int(11)     NOT NULL,
+    `last_name`  varchar(50) NOT NULL,
+    `email`      varchar(50) NOT NULL,
+    `password`   varchar(50) NOT NULL,
+    `created_at` datetime    NOT NULL DEFAULT current_timestamp(),
+    `updated_at` datetime    NOT NULL DEFAULT current_timestamp(),
+    PRIMARY KEY (`id`),
+    KEY `role_id` (`role_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
 
 --
 -- Contraintes pour les tables déchargées
@@ -221,34 +189,34 @@ INSERT INTO `users` (`id`, `first_name`, `role_id`, `last_name`, `email`, `passw
 -- Contraintes pour la table `companies`
 --
 ALTER TABLE `companies`
-  ADD CONSTRAINT `companies_ibfk_1` FOREIGN KEY (`type_id`) REFERENCES `types` (`id`);
+    ADD CONSTRAINT `companies_ibfk_1` FOREIGN KEY (`type_id`) REFERENCES `types` (`id`);
 
 --
 -- Contraintes pour la table `contacts`
 --
 ALTER TABLE `contacts`
-  ADD CONSTRAINT `contacts_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`);
+    ADD CONSTRAINT `contacts_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`);
 
 --
 -- Contraintes pour la table `invoices`
 --
 ALTER TABLE `invoices`
-  ADD CONSTRAINT `invoices_ibfk_1` FOREIGN KEY (`id_company`) REFERENCES `companies` (`id`);
+    ADD CONSTRAINT `invoices_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`);
 
 --
 -- Contraintes pour la table `roles_permission`
 --
 ALTER TABLE `roles_permission`
-  ADD CONSTRAINT `roles_permission_ibfk_1` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`),
-  ADD CONSTRAINT `roles_permission_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
+    ADD CONSTRAINT `roles_permission_ibfk_1` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`),
+    ADD CONSTRAINT `roles_permission_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
 
 --
 -- Contraintes pour la table `users`
 --
 ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
+    ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
