598e47eab13c06fad5001279262ba1dce0448a1a
Nora-H1 <nora.hashani01@gmail.com>
2025-01-30 15:19:11 +0100

    add branch header

--- frontend/src/pages/header.jsx
+++ frontend/src/pages/header.jsx
@@ -3,23 +3,28 @@ import { NavLink } from "react-router-dom";
 function Header() {
     return (
       <header>
-        <div>
-          {/* Logo */}
-          <h1>COGIP</h1>
+        <div className="w-screen p-12 flex flex-row font-cogip-roboto">
+
+          <div className="mr-2 basis-64 justify-start">
+            <h1 className="font-black">COGIP</h1>
+          </div>
+
+          <div className="basis-128">
+            <nav>
+              <ul className="flex flex-row gap-4 justify center">
+                <li className="basis-3xs"><NavLink to="/">Home</NavLink></li>
+                <li className="basis-2xs"><NavLink to="/invoices" >Invoices</NavLink></li>
+                <li className="basis-xs"><NavLink to="/companies">Companies</NavLink></li>
+                <li className="basis-sm"><NavLink to="/contacts">Contacts</NavLink></li>
+              </ul>
+            </nav>
+          </div>
   
-        <nav>
-          <ul>
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/invoices" >Invoices</NavLink></li>
-            <li><NavLink to="/companies">Companies</NavLink></li>
-            <li><NavLink to="/contacts">Contacts</NavLink></li>
-          </ul>
-        </nav>
-  
-          <div>
-            <button>Sign up</button>
+          <div className="relative right-0 flex basis-64 gap-4 justify-end">
+            <a href="#">Sign up</a>
             <a href="#">Login</a>
           </div>
+
         </div>
       </header>
     );
