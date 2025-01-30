9b8af3eafea2ab6d7c64ccde518c50a910a01172
Clean-Pick <benjamin.will98@gmail.com>
2025-01-30 15:35:54 +0100

    header """fix"""

--- frontend/src/pages/header.jsx
+++ frontend/src/pages/header.jsx
@@ -1,34 +1,34 @@
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 function Header() {
     return (
-      <header>
-        <div className="w-screen p-12 flex flex-row font-cogip-roboto">
+        <header className="relative w-screen pl-30 pt-12 pb-12 flex font-cogip-roboto font-black bg-cogip-yellow">
 
-          <div className="mr-2 basis-64 justify-start">
-            <h1 className="font-black">COGIP</h1>
-          </div>
+            <div className="mr-2 basis-64 justify-self-start align-middle">
+                <h1 className="font-black text-4xl">COGIP</h1>
+            </div>
 
-          <div className="basis-128">
-            <nav>
-              <ul className="flex flex-row gap-4 justify center">
-                <li className="basis-3xs"><NavLink to="/">Home</NavLink></li>
-                <li className="basis-2xs"><NavLink to="/invoices" >Invoices</NavLink></li>
-                <li className="basis-xs"><NavLink to="/companies">Companies</NavLink></li>
-                <li className="basis-sm"><NavLink to="/contacts">Contacts</NavLink></li>
-              </ul>
-            </nav>
-          </div>
-  
-          <div className="relative right-0 flex basis-64 gap-4 justify-end">
-            <a href="#">Sign up</a>
-            <a href="#">Login</a>
-          </div>
+            <div className="flex basis-128 justify-self-center align-middle items-center">
+                <nav>
+                    <ul className="flex gap-4 text-xl">
+                        <li className="basis-3xs"><NavLink to="/">Home</NavLink></li>
+                        <li className="basis-2xs"><NavLink to="/invoices">Invoices</NavLink></li>
+                        <li className="basis-xs"><NavLink to="/companies">Companies</NavLink></li>
+                        <li className="basis-sm"><NavLink to="/contacts">Contacts</NavLink></li>
+                    </ul>
+                </nav>
+            </div>
 
-        </div>
-      </header>
+            <div
+                className="absolute right-30 flex basis-64 gap-4 justify-self-end align-middle items-center text-xl">
+                <a href="#">Sign up</a>
+                <a href="#">Login</a>
+            </div>
+
+
+        </header>
     );
-  }
-  
-  export default Header;
+}
+
+export default Header;
   
