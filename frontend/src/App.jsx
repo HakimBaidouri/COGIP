8e9711e986b615093f5f078ba86f93bb212521ed
Thibaut Julien <thbtjulien21@gmail.com>
2025-01-30 09:27:56 +0100

    mise en place component nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,24 +2,14 @@ import { useState } from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
 import Header from "./pages/header.jsx";
-import Invoices from "./pages/invoices.jsx";
-import Home from "./pages/home.jsx";
-import Contacts from "./pages/contacts.jsx";
-import Companies from "./pages/companies.jsx";
+import Nav from "./components/nav.jsx";
 
 function App() {
   return (
     <Router>
-      <Header />  
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/invoices" element={<Invoices />} />
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/companies" element={<Companies />} />
-      </Routes>
+      <Header />
+      <Nav />
     </Router>
   );
 }
