5be168b968b8fac85f06cfbfe7eb3be0216b514b
Nora-H1 <nora.hashani01@gmail.com>
2025-01-30 13:30:06 +0100

    update footer in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./pages/header.jsx";
 import Nav from "./components/nav.jsx";
+import Footer from "./pages/footer.jsx";
 
 function App() {
   return (
     <Router>
       <Header />
       <Nav />
+      <Footer /> 
     </Router>
   );
 }
