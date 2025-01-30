c97c9b84e7adfe6cc8c1893fa0f597d63230ee72
Clean-Pick <benjamin.will98@gmail.com>
2025-01-30 10:05:01 +0100

    Homepage done (without modules)

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -4,17 +4,34 @@ function Home() {
             <section
                 className="bg-cogip-yellow  h-screen w-screen">
 
-                <div className="p-30 flex items-center font-black leading-16 font-cogip-inter align-middle">
-                    <div className=" flex-1 text-7xl ">MANAGE YOUR
+                <div className="p-30 flex items-center font-black leading-[78] font-cogip-inter align-middle">
+                    <div className=" flex-1 text-7xl uppercase">MANAGE YOUR
                         CUSTOMERS AND
                         INVOICES EASILY
                     </div>
                     <img src="/index/index_illustration.png" className="flex-1 text-8xl " alt="coder hipster">
                     </img>
                 </div>
+
             </section>
 
-            <section>
+            
+            <section
+                className="h-[550px] w-screen relative overflow-hidden">
+
+                <div className="p-30 flex items-center font-black leading-[78] font-cogip-inter align-middle">
+                    <div className=" flex-1 text-7xl uppercase">work better in your company
+                    </div>
+                    <div className="flex-2 flex align-middle items-center justify-center">
+                        <img src="/index/index_illustration_2.png" className=" h-[384px] w-auto" alt="phone">
+                        </img>
+                    </div>
+
+                    <div
+                        className="absolute bottom-[-400px] right-0 w-[1200px] h-[700px] bg-cogip-yellow  transform rotate-[16deg] z-[-10]">
+
+                    </div>
+                </div>
 
             </section>
         </div>
