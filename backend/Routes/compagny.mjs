2fc3b3c5e7ce433e5278db676b75b2e2b1f9a061
vincianenahimana <vnahimana@gmail.com>
2025-01-31 16:22:25 +0100

    MODIFY : add missed methods

--- backend/Routes/compagny.mjs
+++ backend/Routes/compagny.mjs
@@ -21,7 +21,7 @@ router.get('/', (req, res) => {
         error: "Error occurred while retrieving Companies."
       });
     }
-    res.status(200).json(results);
+   return res.status(200).json(results);
 
   })
 }
@@ -31,9 +31,25 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  const selectCompanyByIdQuery = 'SELECT * FROM companies WHERE id = ?';
+  const selectCompanyByIdQuery = `
+  SELECT 
+    companies.name AS Company_name,
+    types.name AS Company_type,
+    companies.country AS Company_country,
+    companies.tva AS Company_tva, 
+    contacts.id AS Contact_id,
+    contacts.name AS Contact_name, 
+    invoices.id AS Invoice_id,
+    invoices.ref AS Invoice_ref,
+    invoices.created_at AS Invoice_created
+    FROM companies
+    LEFT JOIN types ON companies.type_id=types.id
+    LEFT JOIN contacts ON companies.id = contacts.company_id
+    LEFT JOIN invoices ON companies.id = invoices.company_id
+   WHERE companies.id = ?`;
   connection.query(selectCompanyByIdQuery, [id], (err, results) => {
     if (err) {
+      console.error(err)
       return res.status(500).json({
         error: "Error occurred while retrieving the company."
       });
@@ -43,47 +59,81 @@ router.get('/:id', (req, res) => {
         error: "Company not found."
       })
     }
-    res.status(200).json(results[0]);
-  })
+
+    const company = {
+     company_id : id,
+     name:results[0].Company_name,
+     type:results[0].Company_type,
+     country:results[0].Company_country,
+     tva:results[0].Company_tva,
+     contacts : [],
+     invoices: []
+    }
+
+    const contactsMap = new Map();
+    const invoicesMap = new Map();
+
+    results.forEach((row)=>{
+      contactsMap.set(row.Contact_id, {
+        contact_id : row.Contact_id,
+        contact_name: row.Contact_name
+      });
+    });
+
+    results.forEach((row)=>{
+      invoicesMap.set(row.Invoice_id, {
+        invoice_id: row.Invoice_id,
+        invoice_ref: row.Invoice_ref,
+        invoice_created : row.Invoice_created
+      })
+    });
+
+    company.contacts = Array.from(contactsMap.values());
+    company.invoices = Array.from(invoicesMap.values());
+
+    res.json(company)
+    
+  });
 }) ;
-
-
 
 
 // Create a company
 router.post('/', (req, res) => {
-  const { name, email, phone } = req.body;
+  const { name, type_id, country, tva } = req.body;
   
   //Verify required fields
-  if (!name || !email || !phone) {
+  if (!name || !type_id || !country || !tva) {
     return res.status(400).json({
-      error: "name, email and phone are required !"
+      error: "name, type, country and tva are required !"
     })
   }
   
-  //verify if the email already exist
-  const checkEmailRequest = 'SELECT email FROM companies WHERE email = ?';
-  connection.query(checkEmailRequest,[email],(err, result)=>{
+  //verify if the company already exist
+  const checkCompanyQuery = `
+  SELECT tva 
+  FROM companies 
+  WHERE tva = ? ` ;
+  connection.query(checkCompanyQuery,[tva],(err, result)=>{
     if (err) {
       return res.status(500).json({
-        error: "Error occurred while select the email."
+        error: "Error occurred while checking the company."
       });
     }
     if(result.length>0) {
       return res.status(409).json ({
-        error : "Conflict, Another company has already this email"
+        error : "Conflict, the tva number has been already attribute to another company"
       })
     }
     
-    //if the email doesn't exist insert the company
-    const insertCompanyQuery = 'INSERT INTO companies (name, email, phone) VALUES (?,?,?,?)';
-    connection.query(insertCompanyQuery, [name, email, phone], (err, result) => {
+    //if the company doesn't exist insert the company
+    const insertCompanyQuery = 'INSERT INTO companies (name, type_id, country, tva) VALUES (?,?,?,?)';
+    connection.query(insertCompanyQuery, [name, type_id, country, tva], (err, result) => {
       if (err) {
         return res.status(500).json({
           error: "Error occurred while creating the company."
         });
       }
-      res.status(200).json({
+       return res.status(200).json({
         message: "The company has been successfully created",
         CompanyId: result.insertId
       })
@@ -93,77 +143,81 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   const {id} = req.params;
-  const {name, email, phone} =req.body;
+  const { name, type_id, country, tva } = req.body;
 
   //Verify required fields
-  if (!name || !email || !phone) {
+  if (!name || !type_id || !country || !tva) {
     return res.status(400).json({
-      error : "name, email, phone are required!"
+      error: "name, type, country and tva are required !"
     })
   }
 
-  //Very if the id exists in the database
-  const selectIdQuery = 'SELECT id FROM companies WHERE email= ?';
-  connection.query(selectIdQuery,[email], (err, result)=>{
-    if(err) {
-      return res.status(500).json({
-        error : 'Error during searching the id in the database'
-      })
-    }
+  //Verify if the company exist
+    const checkCompanyQuery = "SELECT id FROM companies WHERE id= ?";
+    connection.query(checkCompanyQuery, [id], (err, result) => {
+      if (err) {
+        return res.status(500).json({
+          error: "Error during searching the company in the database",
+        });
+      }
+  
+      if (result.length === 0) {
+        return res.status(404).json({
+          error: "Company not found",
+        });
+      }
+    });
 
-    if (result.length===0) {
-      return res.status(404).json({
-        error :'Company not found'
-      })
-    }
-  })
+  //Verify if tva are not attribute to another company
 
-  //Very if the email  already attribute to another Company
-  const selectEmailQuery = 'SELECT email FROM companies WHERE email = ? and id != ?';
-  connection.query(selectEmailQuery,[email,id],(err, result)=>{
+  const checkTvaCompanyQuery = `
+  SELECT tva 
+  FROM companies 
+  WHERE tva = ? AND id != ? ` ;
+  connection.query(checkTvaCompanyQuery,[tva, id],(err, result)=>{
     if (err) {
       return res.status(500).json({
-        error: "Error occured while selecting the email"
-      })
+        error: "Error occurred while checking TVA."
+      });
     }
     if(result.length>0) {
-      return res.status(409).json({
-        error: 'The email is already exist'
+      return res.status(409).json ({
+        error : "Conflict, Another company has already exist with the same TVA"
       })
     }
-    //if the email doesn't attribute to another Company, the Company can be updated
-    const updateCompanyQuery = 
-    `UPDATE 
-    companies 
-    SET name = ?, email = ?, phone = ?, updated_at= ? 
-    WHERE id = ?`;
-    connection.query(
-      updateCompanyQuery,
-      [name, email, phone, convertToMySqlDate(Date.now()), id],
-      (err)=>{
-        if (err) {
-          console.log(err)
-          return res.status(500).json({
-            error: "Error occured during updating the Company"
-          })
-        }
 
-        res.status(200).json({
-          message : 'The Company has been successfully updated',
-          updatedId : id
-        })
+     // Update the company
+ const updateCompanyQuery = `UPDATE 
+ companies 
+ SET name = ?, type_id = ?, country = ?, tva = ?, updated_at= ? 
+ WHERE id = ?`;
+ connection.query(
+   updateCompanyQuery,
+   [name, type_id, country, tva, convertToMySqlDate(), id],
+   (err) => {
+     if (err) {
+       console.log(err);
+       return res.status(500).json({
+         error: "Error occured during updating the company",
+       });
+     }
 
-      }
-    )
-  })
+    return res.status(200).json({
+       message: "The company has been successfully updated",
+       updatedId: id,
+     });
+   }
+ );
+ })
 
 
-}
-)
+
+})
 
 //Delete a company
 router.delete('/:id', (req, res) => {
   const {id} = req.params;
+
     //Very if the id exists in the database
     const selectIdQuery = 'SELECT id FROM companies WHERE id= ?';
     connection.query(selectIdQuery,[id], (err, result)=>{
@@ -189,7 +243,7 @@ router.delete('/:id', (req, res) => {
       })
     } else {
 
-      res.status(200).json({
+    res.status(200).json({
         message : 'The company has been delete successfully'
       })
     }
