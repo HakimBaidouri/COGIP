eeebcfeb4017690e2a3a76d610280af080d43ef2
Nora-H1 <nora.hashani01@gmail.com>
2025-01-31 11:02:40 +0100

    update header & change home

--- frontend/src/pages/header.jsx
+++ frontend/src/pages/header.jsx
@@ -1,34 +1,50 @@
-import {NavLink} from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 function Header() {
-    return (
-        <header className="relative w-screen pl-30 pt-12 pb-12 flex font-cogip-roboto font-black bg-cogip-yellow">
+  const location = useLocation(); 
 
-            <div className="mr-2 basis-64 justify-self-start align-middle">
-                <h1 className="font-black text-4xl">COGIP</h1>
+  return (
+    <header className="w-screen font-cogip-roboto font-black bg-cogip-yellow pt-15 overflow-hidden">
+      <div className="container mx-auto flex items-center justify-between pl-1">
+        
+        <h1 className="text-5xl font-black">COGIP</h1>
+
+        <nav>
+          <ul className="flex gap-7 text-lg font-bold pr-60">
+            <li><NavLink to="/" className="p-2 border-2 border-transparent hover:border-black">Home</NavLink></li>
+            <li><NavLink to="/invoices" className="p-2 border-2 border-transparent hover:border-black">Invoices</NavLink></li>
+            <li><NavLink to="/companies" className="p-2 border-2 border-transparent hover:border-black">Companies</NavLink></li>
+            <li><NavLink to="/contacts" className="p-2 border-2 border-transparent hover:border-black">Contacts</NavLink></li>
+          </ul>
+        </nav>
+
+        <div className="flex gap-3 items-center text-lg font-bold pr-7">
+          <a href="#" className="px-2 rounded-lg hover:bg-white text-black transition">Sign up</a>
+          <a href="#" className="px-2 rounded-lg hover:bg-white text-black transition">Login</a>
+        </div>
+      </div>
+
+      {/* change header -> page home*/}
+      {location.pathname === "/" && (
+        <section className="bg-cogip-yellow h-130 w-screen">
+          <div className="pl-23 flex items-center font-black font-cogip-inter align-middle">
+            <div className="flex-1 text-7xl uppercase">
+              MANAGE YOUR CUSTOMERS AND INVOICES EASILY
             </div>
+            <img
+              src="/index/index_illustration.png"
+              className="flex-1 text-8xl"
+              alt="coder hipster"
+            />
+          </div>
+        </section>
+      )}
 
-            <div className="flex basis-128 justify-self-center align-middle items-center">
-                <nav>
-                    <ul className="flex gap-4 text-xl">
-                        <li className="basis-3xs"><NavLink to="/">Home</NavLink></li>
-                        <li className="basis-2xs"><NavLink to="/invoices">Invoices</NavLink></li>
-                        <li className="basis-xs"><NavLink to="/companies">Companies</NavLink></li>
-                        <li className="basis-sm"><NavLink to="/contacts">Contacts</NavLink></li>
-                    </ul>
-                </nav>
-            </div>
+       {/*Img*/}
+       <img src="/index/header_rectangle.svg" alt="white rectangle" className="relative scale-107 bottom-[-25px] pl-27" />
 
-            <div
-                className="absolute right-30 flex basis-64 gap-4 justify-self-end align-middle items-center text-xl">
-                <a href="#">Sign up</a>
-                <a href="#">Login</a>
-            </div>
-
-
-        </header>
-    );
+    </header> 
+  );
 }
 
 export default Header;
-  
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,21 +1,7 @@
 function Home() {
     return (
         <div>
-            <section
-                className="bg-cogip-yellow  h-screen w-screen">
-
-                <div className="p-30 flex items-center font-black leading-[78] font-cogip-inter align-middle">
-                    <div className=" flex-1 text-7xl uppercase">MANAGE YOUR
-                        CUSTOMERS AND
-                        INVOICES EASILY
-                    </div>
-                    <img src="/index/index_illustration.png" className="flex-1 text-8xl " alt="coder hipster">
-                    </img>
-                </div>
-
-            </section>
-
-            
+                        
             <section
                 className="h-[550px] w-screen relative overflow-hidden">
 
