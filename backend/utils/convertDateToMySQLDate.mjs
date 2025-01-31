35ac5fb67ba1bd145ff068ecbf18fb9e425da7ee
vincianenahimana <vnahimana@gmail.com>
2025-01-31 16:23:12 +0100

    MODIFY: Fix the local time

--- backend/utils/convertDateToMySQLDate.mjs
+++ backend/utils/convertDateToMySQLDate.mjs
@@ -1,6 +1,24 @@
-export function convertToMySqlDate(date) {
+export function convertToMySqlDate() {
+    const localDate = new Date();
 
-    const convertDate = new Date(date).toISOString().slice(0,19).replace('T',' ');
-    return convertDate
+    const localDateString = localDate.toLocaleString('en-GB', {
+            year: 'numeric',
+            month: '2-digit',
+            day:'2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second:'2-digit',
+            hour12:false
+         }
+    )
 
+    const [datePart, timePart] = localDateString.split(', ');
+
+    const [day, month, year] = datePart.split('/');
+    const [hour, minute, second] = timePart.split(':');
+
+    const formattedMySQLFormat = `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+
+    return formattedMySQLFormat
 }
+
