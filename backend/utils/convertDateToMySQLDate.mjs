cc94d4b9897bc9b065c1b4e9457fa3bd656ce832
vincianenahimana <vnahimana@gmail.com>
2025-01-31 10:55:43 +0100

    MODIFY:  remove console.log

--- backend/utils/convertDateToMySQLDate.mjs
+++ backend/utils/convertDateToMySQLDate.mjs
@@ -1,8 +1,6 @@
 export function convertToMySqlDate(date) {
 
     const convertDate = new Date(date).toISOString().slice(0,19).replace('T',' ');
-
-    console.log(convertDate)
     return convertDate
 
 }
