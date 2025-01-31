fba6810c73ed119d9b629eb84b3a556839105f0d
Clean-Pick <benjamin.will98@gmail.com>
2025-01-31 11:15:40 +0100

    Home refactor

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -44,19 +44,6 @@ const columnsSmall = [
 function Home() {
     return (
         <div className="w-screen overflow-hidden">
-            <section
-                className="bg-cogip-yellow  h-screen w-screen">
-
-                <div className="p-30 flex items-center font-black leading-[78] font-cogip-inter align-middle">
-                    <div className=" flex-1 text-7xl uppercase">MANAGE YOUR
-                        CUSTOMERS AND
-                        INVOICES EASILY
-                    </div>
-                    <img src="/index/index_illustration.png" className="flex-1 text-8xl " alt="coder hipster">
-                    </img>
-                </div>
-
-            </section>
 
             <Datalist
                 title={"last invoices"}
