0e699825d42fd39db99b140059600f3b3ecc89f5
Clean-Pick <benjamin.will98@gmail.com>
2025-02-07 11:04:15 +0100

    Test successfull, now profit

--- frontend/src/pages/companies.jsx
+++ frontend/src/pages/companies.jsx
@@ -3,19 +3,7 @@ import CompaniesGet from "../components/queries/companiesGet.jsx";
 
 function Companies() {
     return (
-
         <CompaniesGet/>
-
-        // <div>
-        //     <Datalist
-        //         title={"All companies"}
-        //         columns={"CompaniesGet"}
-        //         dataType="companies"
-        //         decorationBar={true}
-        //         hidePagination={false}
-        //         hideSearchBar={false}
-        //     />
-        // </div>
     );
 }
 
